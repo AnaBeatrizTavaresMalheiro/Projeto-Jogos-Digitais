b7124389ded424d9054f68cc48bbf717f0eb2b0f
FelipeOrlandoLanzara <felipeorlandolanzara@gmail.com>
2025-04-17 20:55:19 -0300

    adicionando musica menu

--- project/Chrono X/Assets/Scripts/MainMenu.cs
+++ project/Chrono X/Assets/Scripts/MainMenu.cs
@@ -1,20 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
     void Start() {
-        
+        MusicManager.Instance.PlayMusic("MainMenu"); // toca a musica do menu
     }
 
     void Update() {
         
     }
 
-    public void Play() {
-        SceneManager.LoadScene("Pre Historia");
+    public void Play() { // quando clicar no botao de Play:
+        SceneManager.LoadScene("Pre Historia"); // troca de cena
+        MusicManager.Instance.StopMusic(); // para de tocar a musica
     }
 
-    public void Quit() {
+    public void Quit() { // quando clicar no botao de Quit:
         Application.Quit();
     }
 
